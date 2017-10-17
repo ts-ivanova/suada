dcaf5b3cea13642938fc3a4f003bebf5a36948bd
tsveti-7 <tivanova@fe003.local>
2017-10-17 16:53:43 +0300

    New address for db server

--- python/databaseconfig.py
+++ python/databaseconfig.py
@@ -1,2 +1,2 @@
-dev  = {'host' : 'cn001',      'user': 'meteo', 'passwd': 'xxxxxx', 'db': 'meteodb'}
-prod = {'host' : '10.1.1.220', 'user': 'meteo', 'passwd': 'xxxxxx', 'db': 'suada_4'}
+dev  = {'host' : '10.1.1.252',      'user': 'meteo', 'passwd': 'xxxxxx', 'db': 'meteodb'}
+prod = {'host' : '10.1.1.252', 'user': 'meteo', 'passwd': 'xxxxxx', 'db': 'suada_4'}

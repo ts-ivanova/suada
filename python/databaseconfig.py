7d951a8a47c7575cfcfea9296b2e8fb2c6723aac
tsveti-7 <ts.ivanova7@abv.bg>
2017-11-22 16:45:12 +0000

    Update databaseconfig.py

--- python/databaseconfig.py
+++ python/databaseconfig.py
@@ -1,2 +1,2 @@
 prod  = {'host' : '10.1.1.252',      'user': 'meteo', 'passwd': 'xxxxxx', 'db': 'meteodb'}
-dev = {'host' : '10.1.1.252', 'user': 'meteo', 'passwd': 'm3t30d4t4', 'db': 'dev_suada_4'}
+dev = {'host' : '10.1.1.252', 'user': 'meteo', 'passwd': 'xxxxxx', 'db': 'dev_suada_4'}

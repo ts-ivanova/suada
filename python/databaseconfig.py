f41ccecdc7bec57a909a8677210be59a46ab73dd
tsveti-7 <ts.ivanova7@abv.bg>
2018-05-21 21:15:20 +0300

    Update databaseconfig.py after Physon Migration

--- python/databaseconfig.py
+++ python/databaseconfig.py
@@ -1,3 +1,3 @@
 prod  = {'host' : 'fs002',      'user': 'meteo', 'passwd': 'xxxxxx', 'db': 'meteodb'}
 dev = {'host' : 'fs002', 'user': 'meteo', 'passwd': 'xxxxxx', 'db': 'dev_suada_4'}
-#dev_suada_4 is a backup copy of the database used for tests and work.
+#dev_suada_4 is a backup copy of the database that is used for tests and work.

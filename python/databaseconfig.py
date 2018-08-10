2cc5cacd4661929c4703d57cd91dadb1101a3ae1
tsveti-7 <tivanova@physon.phys.uni-sofia.bg>
2018-08-10 17:06:47 +0300

    pass hidden

--- python/databaseconfig.py
+++ python/databaseconfig.py
@@ -1,3 +1,3 @@
-prod  = {'host' : 'fs002',      'user': 'meteo', 'passwd': 'm3t30d4t4', 'db': 'meteodb'}
-dev = {'host' : 'fs002', 'user': 'meteo', 'passwd': 'm3t30d4t4', 'db': 'dev_suada_4'}
+prod  = {'host' : 'fs002',      'user': 'meteo', 'passwd': 'xxxx', 'db': 'meteodb'}
+dev = {'host' : 'fs002', 'user': 'meteo', 'passwd': 'xxxx', 'db': 'dev_suada_4'}
 #dev_suada_4 is a backup copy of the database that is used for tests and work.

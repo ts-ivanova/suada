c9145f299f8541b9a6799fb83dec8dff09529ff0
Stoyan Pisov <stoyan@fe003.local>
2018-08-23 14:31:22 +0300

    Move station data processing into separate function process_station

--- python/ncdf2db.py
+++ python/ncdf2db.py
@@ -24,13 +24,13 @@ import numpy as np
 def getstations(cur, source_name, country, instrument_name):
     stations=[]
     try:
-        if not country:
             cur.execute("select st.ID, \
                 st.Name, \
                 crd.Longitude, \
                 crd.Latitude, \
                 crd.Altitude, \
                 sen.ID, \
+		st.Country \
                 from SENSOR as sen left join SOURCE as so ON so.ID = sen.SourceID \
                 left join STATION as st ON st.ID = sen.StationID \
                 left join COORDINATE as crd ON crd.STationID = st.ID \
@@ -39,37 +39,6 @@ def getstations(cur, source_name, country, instrument_name):
                 AND instr.Name = %(instrument_name)s", 
                 {
                     'source_name' : source_name,
-                    'instrument_name' : instrument_name
-                })
-            rows =  cur.fetchall()
-
-            if len(rows):
-                for row in rows:
-                    stations.append({'id':row[0],
-                                     'name':row[1],
-                                     'long':row[2], 
-                                     'latt':row[3],
-                                     'alt':row[4],
-                                     'senid':row[5],
-                                     'country':row[6]})
-        if country:
-            cur.execute("select st.ID, \
-                st.Name, \
-                crd.Longitude, \
-                crd.Latitude, \
-                crd.Altitude, \
-                sen.ID, \
-                st.Country \
-                from SENSOR as sen left join SOURCE as so ON so.ID = sen.SourceID \
-                left join STATION as st ON st.ID = sen.StationID \
-                left join COORDINATE as crd ON crd.STationID = st.ID \
-                left join INSTRUMENT as instr ON instr.ID = crd.InstrumentID \
-                WHERE so.Name = %(source_name)s \
-                AND st.Country = %(country)s \
-                AND instr.Name = %(instrument_name)s", 
-                {
-                    'source_name' : source_name,
-                    'country' : country,
                     'instrument_name' : instrument_name
                 })
             rows =  cur.fetchall()
@@ -142,7 +111,150 @@ def get_station_name(cur, country):
     finally:
         return name
 
- 
+
+# Insert model data for station
+def process_station(db, cur, station, ncfile, date):
+	result = True
+	try:
+		stationName = station['name']
+		stationId = station['id']
+		sensorId = station['senid']
+		x0 = station['long']
+		y0 = station['latt']
+		z0 = station['alt']
+		i0 = station['i0']
+		j0 = station['j0']
+		print 'Station: ', station['name'], ' ID: ', station['id'], ' sensorId: ', sensorId
+        	# 1D fields:
+	        T2 = ncfile.variables['T2'][0]
+        	Pressure = ncfile.variables['PSFC'][0]
+	        PBLH = ncfile.variables['PBLH'][0]
+	        HGT = ncfile.variables['HGT'][0]
+	        RAINNC = ncfile.variables['RAINNC'][0]
+	        SNOWNC = ncfile.variables['SNOWNC'][0]
+	        GRAUPELNC = ncfile.variables['GRAUPELNC'][0]
+	        HAILNC = ncfile.variables['HAILNC'][0]
+	        Precipitation = RAINNC + SNOWNC + GRAUPELNC + HAILNC
+	        # 3D fields:
+	        T = ncfile.variables['T'][0]
+	        P = ncfile.variables['P'][0]
+	        PB = ncfile.variables['PB'][0]
+	        PHB = ncfile.variables['PHB'][0]
+	        PH = ncfile.variables['PH'][0]
+	        QVAPOR = ncfile.variables['QVAPOR'][0]
+
+		# Import 1D fields
+                press = Pressure[i0][j0]
+                heigth = HGT[i0][j0]
+                zhd = (0.0022768*(float(press)))/(1-0.00266*np.cos(2*(float(z0))*(3.1416/180))-(0.00028*(float(heigth))/1000))
+                # zhd = zenith hydrostatic delay
+                pblh = PBLH[i0][j0]
+                temp = T2[i0][j0]
+                rain = Precipitation[i0][j0]
+                print('Name: {0} [{1}, {2}, {3}] -> [Temperarture [K]: {4}, Pressure [Pa]: {5}, Rain [mm]: {6}, PBL HEIGHT [m]: {7}, Zenit Heigth Delay [x]: {8}] '
+                      .format(station['name'],
+                              x0,
+                              y0,
+                              z0,
+                              temp,
+                              press,
+                              rain,
+                              pblh,
+                              zhd))
+
+                # SQL commands that insert values of parameters in the tables.
+                # If there is a dublicate, the existing fileds are updated.
+                # 1D data insertion:
+                cur.execute ( "insert into NWP_IN_1D (Datetime, \
+			Temperature, \
+			Pressure, \
+			Altitude, \
+			SensorID, \
+			Latitude, \
+			Longitude, \
+			ZHD, \
+			PBL, \
+			Precipitation)\
+        	        values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s) on duplicate key update\
+	                Temperature = %s,\
+	                Pressure = %s,\
+	                Altitude = %s,\
+	                Latitude = %s,\
+	                Longitude = %s,\
+	                ZHD = %s,\
+	                PBL = %s,\
+	                Precipitation = %s", [date,
+			temp,
+			press,
+			heigth,
+			sensorId,
+			y0,
+			x0,
+			zhd,
+			pblh,
+			rain,
+			temp,
+			press,
+			heigth,
+			y0,
+			x0,
+			zhd,
+			pblh,
+			rain])
+                # 3D data insertion:
+		bottom_top = len(T)
+		# Rd, Cp, Rd_Cp are used for 3D calculation of Tk
+		Rd  = 287.0
+		Cp  = 7.0 * Rd / 2.0
+		Rd_Cp  = Rd / Cp
+                for k in range(0, bottom_top):
+			theta = T[k][i0][j0] + 300.
+			Pair = P[k][i0][j0] + PB[k][i0][j0] # Press3D = Pair
+			tk  = theta * (( Pair/100000. )**(Rd_Cp))
+			QV = QVAPOR[k][i0][j0]
+                    	hgth = (PH[k][i0][j0] + PHB[k][i0][j0])/9.8
+                    
+			cur.execute ( "insert into NWP_IN_3D (Datetime, \
+				Temperature, \
+				Pressure, \
+				SensorID, \
+				Latitude, \
+				Longitude, \
+				Height, \
+				WV_Mixing_ratio, \
+				Level)\
+				values (%s, %s, %s, %s, %s, %s, %s, %s, %s) on duplicate key update\
+				Temperature = %s,\
+				Pressure = %s,\
+				Latitude = %s,\
+				Longitude = %s,\
+				Height = %s,\
+				WV_Mixing_ratio = %s,\
+				Level = %s", [date, 
+				tk,
+				Pair,
+				sensorId,
+				y0,
+				x0,
+				hgth,
+				QV,
+				k,
+				tk,
+				Pair,
+				y0,
+				x0,
+				hgth,
+				QV,
+				k]) # insert or update
+                    
+		db.commit() # commit all data to the specified -d <env>
+
+	except Exception as e:
+		sys.stderr.write('Error occured in process_station: {error}'.format(error = repr(e)))
+	finally:
+
+
+		return result
    
 # Define the main procedure:
 def main(argv):
@@ -157,7 +269,7 @@ def main(argv):
     prefix='wrfout_d02'
     source_name = ''
     All = {'TR', 'MK'}
-    country = All  #possible options are 'BG', 'GR', ...
+    country = 'All'  #possible options are 'BG', 'GR', ...
     env = '' # possible options are 'dev' and 'prod'. Soon txt
     instrument_name = 'GNSS'
     
@@ -180,13 +292,9 @@ def main(argv):
         elif opt in ("-c", "--country"):
 #            country = str(arg)
             if not country:
-                county = All
+                county = 'All'
             if country:
                 country = str(arg)
-#            if len(str(arg)) = 0:
-#                country = All
-#            elif len(str(arg)) != 0:
-#                country = str(arg)
         elif opt in ("-d", "--env"):
             env = str(arg)
 
@@ -242,13 +350,6 @@ def main(argv):
     
     print('Source id: {} found for source name: {}'.format(source_id, source_name))
     
-    # Fetching station names for the selected country ...
-    print('Trying to fetch the station names for the selected country...')
-    name = get_station_name(cur, country)
-    if name < 0:
-        print 'Error: Can not find station names from the selected country: {}'.format(country)
-        sys.exit(1)
-
     # Call the procedure that selects the stations' information from the SUADA information tables:
     # (The SUADA information tables are: INSTRUMENT, STATION, COORDINATE, SENSOR and SOURCE.)
     print('Get stations')
@@ -272,38 +373,14 @@ def main(argv):
         xlong = ncfile.variables['XLONG'][0]
         xlat = ncfile.variables['XLAT'][0]
         alt = ncfile.variables['HGT'][0]
-        T2 = ncfile.variables['T2'][0]
-        Pressure = ncfile.variables['PSFC'][0]
-        PBLH = ncfile.variables['PBLH'][0]
-        HGT = ncfile.variables['HGT'][0]
-        RAINNC = ncfile.variables['RAINNC'][0]
-        SNOWNC = ncfile.variables['SNOWNC'][0]
-        GRAUPELNC = ncfile.variables['GRAUPELNC'][0]
-        HAILNC = ncfile.variables['HAILNC'][0]
-        Precipitation = RAINNC + SNOWNC + GRAUPELNC + HAILNC
         south_north = len(xlong)
         west_east = len(xlong[0])
-        # Above are most of the used parameters
-        # 3D fields:
-        T = ncfile.variables['T'][0]
-        P = ncfile.variables['P'][0]
-        PB = ncfile.variables['PB'][0]
-        PHB = ncfile.variables['PHB'][0]
-        PH = ncfile.variables['PH'][0]
-        QVAPOR = ncfile.variables['QVAPOR'][0]
-        bottom_top = len(T)
-        print('bottom_top = {}'.format(bottom_top))
-        
-        Rd          = 287.0
-        Cp          = 7.0*Rd / 2.0
-        Rd_Cp       = Rd / Cp
-        # Rd, Cp, Rd_Cp are used for 3D calculation of Tk
         
         for station in stations:
             stationName = station['name']
             stationId = station['id']
             sensorId = station['senid']
-            print 'Station: ', station['name'], ' ID: ', station['id'], ' sensorId: ', sensorId
+            print 'Station: ', station['name'], ' ID: ', station['id'], ' sensorId: ', sensorId, 'Country Code: ', station['country']
             x0 = station['long']
             y0 = station['latt']
             z0 = station['alt']
@@ -330,57 +407,9 @@ def main(argv):
                 station['i0'] = i0
                 station['j0'] = j0
             
-            if i0 > -1 and j0 > -1:
-                press = Pressure[i0][j0]
-                heigth = HGT[i0][j0]
-                zhd = (0.0022768*(float(press)))/(1-0.00266*np.cos(2*(float(z0))*(3.1416/180))-(0.00028*(float(heigth))/1000))
-                # zhd = zenith hydrostatic delay
-                pblh = PBLH[i0][j0]
-                temp = T2[i0][j0]
-                rain = Precipitation[i0][j0]
-                print('Name: {0} [{1}, {2}, {3}] -> [Temperarture [K]: {4}, Pressure [Pa]: {5}, Rain [mm]: {6}, PBL HEIGHT [m]: {7}, Zenit Heigth Delay [x]: {8}] '
-                      .format(station['name'],
-                              xlong[i0][j0],
-                              xlat[i0][j0],
-                              alt[i0][j0],
-                              temp,
-                              press,
-                              rain,
-                              pblh,
-                              zhd))
+            if (i0 > -1 and j0 > -1) and ( (country == 'All') or (country == station['country']) ):
+		process_station(db, cur, station, ncfile, date)
 
-                # SQL commands that insert values of parameters in the tables.
-                # If there is a dublicate, the existing fileds are updated.
-                # 1D data insertion:
-                cur.execute ( "insert into NWP_IN_1D (Datetime, Temperature, Pressure, Altitude, SensorID, Latitude, Longitude, ZHD, PBL, Precipitation)\
-                values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s) on duplicate key update\
-                Temperature = %s,\
-                Pressure = %s,\
-                Altitude = %s,\
-                Latitude = %s,\
-                Longitude = %s,\
-                ZHD = %s,\
-                PBL = %s,\
-                Precipitation = %s", [date, temp, press, heigth, sensorId, y, x, zhd, pblh, rain, temp, press, heigth, y, x, zhd, pblh, rain])
-                # 3D data insertion:
-                for k in range(0, bottom_top):
-                    theta = T[k][i0][j0] + 300.
-                    Pair = P[k][i0][j0] + PB[k][i0][j0] # Press3D = Pair
-                    tk  = theta * (( Pair/100000. )**(Rd_Cp))
-                    QV = QVAPOR[k][i0][j0]
-                    hgth = (PH[k][i0][j0] + PHB[k][i0][j0])/9.8
-                    
-                    cur.execute ( "insert into NWP_IN_3D (Datetime, Temperature, Pressure, SensorID, Latitude, Longitude, Height, WV_Mixing_ratio, Level)\
-                    values (%s, %s, %s, %s, %s, %s, %s, %s, %s) on duplicate key update\
-                    Temperature = %s,\
-                    Pressure = %s,\
-                    Latitude = %s,\
-                    Longitude = %s,\
-                    Height = %s,\
-                    WV_Mixing_ratio = %s,\
-                    Level = %s", [date, tk, Pair, sensorId, y, x, hgth, QV, k, tk, Pair, y, x, hgth, QV, k]) # insert or update
-                    
-            db.commit() # commit all data to the specified -d <env>
     if not(len(flist)):
         print 'No candidates for import files found ...'
         sys.exit(1)

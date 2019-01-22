6f411c7061ba9afe256ef81dad2426bfdffd7e38
tsveti-7 <tivanova@cn006.local>
2019-01-22 11:43:53 +0200

    switched to suada_5 database

--- python/databaseconfig.py
+++ python/databaseconfig.py
@@ -1,3 +1,3 @@
 prod  = {'host': 'fs002', 'user': 'meteo', 'passwd': 'xxxx', 'db': 'meteodb'}
-dev = {'host': 'fs002', 'user': 'meteo', 'passwd': 'xxxx', 'db': 'dev_suada_4'}
+dev = {'host': 'fs002', 'user': 'meteo', 'passwd': 'xxxx', 'db': 'suada_5'}
 #dev_suada_4 is a backup copy of the suada database and is used for tests and work.
